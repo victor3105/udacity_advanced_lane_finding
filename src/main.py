83f519c73bbf4ffef210233d26c4d0c7c5d904f0
victor3105 <glebov_v94@mail.ru>
2020-03-14 23:51:27 -0600

    added image thresholding

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import glob
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
+
 def calibrate_camera(images_path):
     images = glob.glob(images_path)
 
@@ -31,6 +32,7 @@ def calibrate_camera(images_path):
     ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
     return mtx, dist
 
+
 def undistort(mtx, dist, frame_path):
     test_img_name = frame_path
     test_img = mpimg.imread(test_img_name)
@@ -39,9 +41,45 @@ def undistort(mtx, dist, frame_path):
 
     return undist
 
+
+def threshold(img, s_thresh=(180, 255), sx_thresh=(50, 100)):
+    img = np.copy(img)
+    # Convert to HLS color space and separate the V channel
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    l_channel = hls[:, :, 1]
+    s_channel = hls[:, :, 2]
+    # Sobel x
+    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0)  # Take the derivative in x
+    abs_sobelx = np.absolute(sobelx)  # Absolute x derivative to accentuate lines away from horizontal
+    scaled_sobel = np.uint8(255 * abs_sobelx / np.max(abs_sobelx))
+
+    # Threshold x gradient
+    sxbinary = np.zeros_like(scaled_sobel)
+    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1
+
+    # Threshold color channel
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    combined_binary = np.zeros_like(sxbinary)
+    combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
+    # # Stack each channel
+    # color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+    return combined_binary
+
+# test the pipeline here
 images = '../data/camera_cal/calibration*.jpg'
 test_img = '../data/test_images/straight_lines1.jpg'
+undistorted_img = '../data/output_images/undostorted.jpg'
+bin_img_name = '../data/output_images/thresholded.jpg'
+
 mtx, dist = calibrate_camera(images)
 undist_img = undistort(mtx, dist, test_img)
-plt.imshow(undist_img)
+bin_img = threshold(undist_img)
+# plt.imshow(undist_img)
+# plt.show()
+# undist_img = cv2.cvtColor(undist_img, cv2.COLOR_RGB2BGR)
+# cv2.imwrite(undistorted_img, undist_img)
+# cv2.imwrite(bin_img_name, bin_img * 255)
+bin_half = bin_img[bin_img.shape[0] // 2:, :]
+plt.imshow(bin_half)
 plt.show()

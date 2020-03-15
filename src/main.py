a261aca33850f3a96d0c1e05d7b1c89ad3ca6419
victor3105 <glebov_v94@mail.ru>
2020-03-15 10:07:21 -0600

    added perspective transform

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ def undistort(mtx, dist, frame_path):
     return undist
 
 
-def threshold(img, s_thresh=(180, 255), sx_thresh=(50, 100)):
+def threshold(img, s_thresh=(180, 255), sx_thresh=(30, 100)):
     img = np.copy(img)
     # Convert to HLS color space and separate the V channel
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
@@ -66,20 +66,75 @@ def threshold(img, s_thresh=(180, 255), sx_thresh=(50, 100)):
     # color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
     return combined_binary
 
+
+def warp(bin_img):
+    # src coordinates for perspective transform
+    x1 = 190
+    y1 = bin_img.shape[0]
+    x2 = 590
+    y2 = 450
+    x3 = 690
+    y3 = 450
+    x4 = 1120
+    y4 = bin_img.shape[0]
+    src = np.float32(
+        [[x1, y1],
+         [x2, y2],
+         [x3, y3],
+         [x4, y4]])
+
+    # dst coords
+    x1_dst = 350
+    y1_dst = bin_img.shape[0]
+    x2_dst = 350
+    y2_dst = 0
+    x3_dst = 950
+    y3_dst = 0
+    x4_dst = 950
+    y4_dst = bin_img.shape[0]
+    dst = np.float32(
+        [[x1_dst, y1_dst],
+         [x2_dst, y2_dst],
+         [x3_dst, y3_dst],
+         [x4_dst, y4_dst]])
+
+    img_size = (bin_img.shape[1], bin_img.shape[0])
+
+    src_draw = np.array(
+        [[x1, y1],
+         [x2, y2],
+         [x3, y3],
+         [x4, y4]])
+    dst_draw = np.array(
+        [[x1_dst, y1_dst],
+         [x2_dst, y2_dst],
+         [x3_dst, y3_dst],
+         [x4_dst, y4_dst]])
+
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(bin_img, M, img_size, flags=cv2.INTER_LINEAR)
+    # check if the coordinates are correct
+    # color_img = np.dstack((bin_img, np.zeros_like(bin_img), np.zeros_like(bin_img))) * 255
+    # color_img = cv2.polylines(color_img, [src_draw], True, (0, 255, 0))
+    # cv2.imshow('src', color_img)
+    # cv2.waitKey()
+    return warped
+
+
 # test the pipeline here
 images = '../data/camera_cal/calibration*.jpg'
-test_img = '../data/test_images/straight_lines1.jpg'
-undistorted_img = '../data/output_images/undostorted.jpg'
+test_img_name = '../data/test_images/straight_lines1.jpg'
+undistorted_img_name = '../data/output_images/undostorted.jpg'
 bin_img_name = '../data/output_images/thresholded.jpg'
+warped_img_name = '../data/output_images/warped.jpg'
 
 mtx, dist = calibrate_camera(images)
-undist_img = undistort(mtx, dist, test_img)
-bin_img = threshold(undist_img)
+undist_img = undistort(mtx, dist, test_img_name)
+binary_img = threshold(undist_img)
+warped = warp(binary_img)
 # plt.imshow(undist_img)
 # plt.show()
 # undist_img = cv2.cvtColor(undist_img, cv2.COLOR_RGB2BGR)
 # cv2.imwrite(undistorted_img, undist_img)
-# cv2.imwrite(bin_img_name, bin_img * 255)
-bin_half = bin_img[bin_img.shape[0] // 2:, :]
-plt.imshow(bin_half)
-plt.show()
+# cv2.imwrite(bin_img_name, binary_img * 255)
+# cv2.imwrite(warped_img_name, warped * 255)
